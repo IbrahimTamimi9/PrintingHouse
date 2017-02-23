c2c5165693d01347d073cd9aa7bbacfa66109335
Roman Mizin <r.mizin96@gmail.com>
2017-02-23 19:40:24 +0200

    1. Added check, if device is connected to the internet or not.

--- Avalon-print/HomeVC.swift
+++ Avalon-print/HomeVC.swift
@@ -11,6 +11,7 @@ import CoreData
 import JTMaterialTransition
 import FirebaseAuth
 import FirebaseDatabase
+import SystemConfiguration
 
 var rightBarButton: ENMBadgedBarButtonItem?
 var leftBarButton: ENMBadgedBarButtonItem?
@@ -45,6 +46,62 @@ let screenSize: CGRect = UIScreen.main.bounds
  }
 
 
+
+protocol Utilities {
+  
+}
+
+extension NSObject: Utilities{
+  
+  enum ReachabilityStatus {
+    case notReachable
+    case reachableViaWWAN
+    case reachableViaWiFi
+  }
+  
+  
+  var currentReachabilityStatus: ReachabilityStatus {
+    
+    var zeroAddress = sockaddr_in()
+    zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+    zeroAddress.sin_family = sa_family_t(AF_INET)
+    
+    guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+      $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+        SCNetworkReachabilityCreateWithAddress(nil, $0)
+      }
+    }) else {
+      return .notReachable
+    }
+    
+    var flags: SCNetworkReachabilityFlags = []
+    if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+      return .notReachable
+    }
+    
+    if flags.contains(.reachable) == false {
+      // The target host is not reachable.
+      return .notReachable
+    }
+    else if flags.contains(.isWWAN) == true {
+      // WWAN connections are OK if the calling application is using the CFNetwork APIs.
+      return .reachableViaWWAN
+    }
+    else if flags.contains(.connectionRequired) == false {
+      // If the target host is reachable and no connection is required then we'll assume that you're on Wi-Fi...
+      return .reachableViaWiFi
+    }
+    else if (flags.contains(.connectionOnDemand) == true || flags.contains(.connectionOnTraffic) == true) && flags.contains(.interventionRequired) == false {
+      // The connection is on-demand (or on-traffic) if the calling application is using the CFSocketStream or higher APIs and no [user] intervention is needed
+      return .reachableViaWiFi
+    }
+    else {
+      return .notReachable
+    }
+  }
+  
+}
+
  class HomeVC: UIViewController {
   
     @IBOutlet weak var backgroundImageView: UIImageView!
@@ -75,6 +132,9 @@ let screenSize: CGRect = UIScreen.main.bounds
 //        print ("Error signing out: %@", signOutError)
 //      }
       
+    checkInternetConnection()
+   //   print("INEEEEEEEEEEEEEEEET", currentReachabilityStatus != .notReachable) //true connected
+      
         bucketTransition = JTMaterialTransition(animatedView: button)
         profileButtonTransition = JTMaterialTransition(animatedView: profileButton)
       
@@ -86,6 +146,27 @@ let screenSize: CGRect = UIScreen.main.bounds
         registerCell()
         getVariablesFromJSON()
      
+  }
+  
+  func checkInternetConnection () {
+    
+    if  currentReachabilityStatus != .notReachable {
+      //true connected
+      
+      
+    } else {
+      //no internet
+      let alertController = UIAlertController(title: "Ошибка подключения к интернету", message: "Для совершения покупок, пожалуйста, подключитесь к интернету.", preferredStyle: .alert)
+      
+      let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+      
+      alertController.addAction(defaultAction)
+      
+      self.present(alertController, animated: true, completion: nil)
+
+      
+    }
+    
   }
 
   
